50c42ce3bab77bea65a00678b9bd8fbfcc099683
Hong Ong <Hong Ong>
2023-02-09 12:06:18 +0700

    Ingest MySQL to bronze

--- ecom_analytics/models/bronze/olist_order_items.sql
+++ ecom_analytics/models/bronze/olist_order_items.sql
@@ -8,10 +8,10 @@
 
 SELECT
     order_id
-    ,order_item_id
-    ,product_id
-    ,seller_id
-    ,shipping_limit_date
-    ,price
-    ,freight_value
+    , order_item_id
+    , product_id
+    , seller_id
+    , shipping_limit_date
+    , price
+    , freight_value
 FROM de_mysql.brazillian_ecommerce.olist_order_items_dataset
